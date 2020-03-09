4a02d47f0e4ccecbc64e7e4b2716344404e668af
Andrew Roberts <andrew.jacob.roberts@gmail.com>
2020-03-09 13:36:28 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center w-screen h-screen text-2xl bg-gray-100">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
